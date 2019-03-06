59603ba6f8a5c8a5c3b580555a0de5e3bfc26e71
riri-jp <alahmary.mashari@gmail.com>
2019-03-06 14:36:44 +0300

    Deployd my web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="header">
+          <div id="nav-wrapper">
+            <nav id="nav" />
+          </div>
+          <div class="container">
+            <div id="logo">
+              <h1>
+                <a href="#"> Eng.Mashari Alahmary </a>
+              </h1>
+              <span class="tag" />
+            </div>
+          </div>
+        </div>
+
+        <div id="featured">
+          <div class="container">
+            <header>
+              <h2> Welcome To My Life😀</h2>
+            </header>
+
+            <hr />
+            <div class="row">
+              <section class="4u">
+                <span class="pennant">
+                  <span class="fa fa-briefcase" />
+                </span>
+                <h3> مشاريعي </h3>
+                <p>
+                  مؤسس موقع ساونيهون لتنظيم الرحلات السياحية و التجارية من والى
+                  اليابان فقط ، والافكار والمشاريع لم تنتهي بعد والقادم اقوى...
+                </p>
+              </section>
+              <section class="4u">
+                <span class="pennant">
+                  <span class="fa fa-lock" />
+                </span>
+                <h3> الخبرات </h3>
+                <p>
+                  {" "}
+                  خبرة سنتان في مدينة الامير سلطان بن عبد العزيز للخدمات
+                  الانسانية كمطور برامج ، العمل مع شركة ارامكو و سوق.كوم كمترجم
+                  لغة يابانية ، العمل في اعمال حرة أخرى
+                </p>
+              </section>
+              <section class="4u">
+                <span class="pennant">
+                  <span class="fa fa-globe" />
+                </span>
+                <h3> التعليم </h3>
+                <p>
+                  {" "}
+                  خريج دولة اليابان تخصص هندسة نظم المعلومات ، حاصل على اللغة
+                  اليابانية{" "}
+                </p>
+              </section>
+            </div>
+          </div>
+        </div>
+
+        <div id="tweet">
+          <div class="container">
+            <section>
+              <blockquote>
+                &ldquo;همتنا مثل جبل طويق، ولن تنكسر إلا إذا انهد هذا الجبل
+                وتساوى بالأرض&rdquo;
+              </blockquote>
+            </section>
+          </div>
+        </div>
+
+        <div id="footer">
+          <div class="container">
+            <section>
+              <header>
+                <h2>ありがとうございます</h2>
+                <span class="byline">شكرا لكم ودمتم سالمين </span>
+              </header>
+              <ul />
+            </section>
+          </div>
+        </div>
       </div>
     );
   }
